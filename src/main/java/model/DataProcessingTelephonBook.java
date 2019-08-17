514c74423a0704f4789e7e3acd21d1dcdb68b02d
@NikolayTraining <ribal_kate@rambler.ru>
2019-08-17 15:24:39 +0300

    инициализирую книгу пользователей и телефонов

--- src/main/java/model/DataProcessingTelephonBook.java
+++ src/main/java/model/DataProcessingTelephonBook.java
@@ -1,4 +1,28 @@
 package model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DataProcessingTelephonBook {
+    private Map<String, List<String>> map = new HashMap<>();
+    private List<String> list;
+    private List<String> list2;
+
+    public DataProcessingTelephonBook() {
+        initBook();
+    }
+
+    private void initBook() {
+        readClientData("Иванов И.И. +8 800 2000 500 +8 800 200 600");
+        readClientData("Петров П.П. +8 800 2000 700");
+        readClientData("Сидоров С.С. +8 800 2000 800 +8 800 2000 900 +8 800 2000 000");
+    }
+
+    private void readClientData(String s) {
+        
+    }
+
+
+
 }
