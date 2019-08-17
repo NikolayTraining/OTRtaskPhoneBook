1a3e296eb1557a14a2ff4f9ced2c9b794ff1a9da
@NikolayTraining <ribal_kate@rambler.ru>
2019-08-17 15:50:48 +0300

    написал метод старта программы main в классе StartProgram

--- src/main/java/projectStart/StartProgram.java
+++ src/main/java/projectStart/StartProgram.java
@@ -1,4 +1,13 @@
 package projectStart;
 
+import view.ConsoleWork;
+import view.ConsoleWorkImpl;
+
 public class StartProgram {
+
+    public static void main(String[] args) {
+        ConsoleWorkImpl consoleWork = new ConsoleWork();
+        consoleWork.enterConsoleSurname();
+        System.out.println("You exit program.");
+    }
 }
--- src/main/java/view/ConsoleWork.java
+++ src/main/java/view/ConsoleWork.java
@@ -1,6 +1,5 @@
 package view;
 
-
 import model.DataProcessingTelephonBook;
 import model.DataProcessingTelephonBookImpl;
 
